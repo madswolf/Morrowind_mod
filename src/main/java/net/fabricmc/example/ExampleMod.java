c099b40274333e44682fc573c954c26d3260cc32
Madswolf <mads@madswolf.dk>
2023-03-10 01:12:58 +0100

    Made the skooma into a consumable food item

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -2,6 +2,9 @@ package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -16,17 +19,23 @@ public class ExampleMod implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
+	public static final String ModName = "tutorial";
 	// an instance of our new item
 	public static final Item CUSTOM_ITEM =
-			Registry.register(Registries.ITEM, new Identifier("tutorial", "dank_kush"),
+			Registry.register(Registries.ITEM, new Identifier(ModName, "dank_kush"),
 					new Item(new FabricItemSettings()));
 
-	public static final Item SKOOMA =
-			Registry.register(Registries.ITEM, new Identifier("tutorial", "skooma"),
-					new Skooma(new FabricItemSettings()));
 
+	public static final FoodComponent SKOOMA_FOODCOMPONENT = new FoodComponent.Builder()
+			.hunger(-1)
+			.saturationModifier(-1)
+			.alwaysEdible()
+			.statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 200), 100)
+			.build();
+	public static final Item SKOOMA = Registry.register(Registries.ITEM, new Identifier(ModName, "skooma"),
+			new Skooma(new FabricItemSettings().food(SKOOMA_FOODCOMPONENT)));
 
-	public static final Identifier SKOOMA_SOUND_ID = new Identifier("tutorial", "skooma_sound");
+	public static final Identifier SKOOMA_SOUND_ID = new Identifier(ModName, "skooma_sound");
 	public static SoundEvent SKOOMA_SOUND = Registry.register(Registries.SOUND_EVENT, ExampleMod.SKOOMA_SOUND_ID, SoundEvent.of(SKOOMA_SOUND_ID));
 
 	@Override
--- src/main/java/net/fabricmc/example/Skooma.java
+++ src/main/java/net/fabricmc/example/Skooma.java
@@ -1,12 +1,22 @@
 package net.fabricmc.example;
 
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsage;
+import net.minecraft.item.PotionItem;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Skooma extends Item {
 
@@ -14,9 +24,34 @@ public class Skooma extends Item {
         super(settings);
     }
 
+    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+    public int getMaxUseTime(ItemStack stack) {
+        return 40;
+    }
     @Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
-        playerEntity.playSound(ExampleMod.SKOOMA_SOUND, 1.0F, 1.0F);
-        return TypedActionResult.success(playerEntity.getStackInHand(hand));
+    public SoundEvent getEatSound(){
+        return ExampleMod.SKOOMA_SOUND;
+    }
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        return ItemUsage.consumeHeldItem(world, user, hand);
+    }
+
+    @Override
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.DRINK;
+    }
+
+    @Override
+    public SoundEvent getDrinkSound() {
+        return ExampleMod.SKOOMA_SOUND;
+    }
+
+    @Override
+     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        //user.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 200));
+
+        return super.finishUsing(stack,world,user);
     }
 }
