912a8acb39f739730c530a3a6f60b577fc2063e5
Madswolf <mads@madswolf.dk>
2023-03-12 05:18:01 +0100

    Added MoonShineCollector block - needs fluid and fluid generation

---
+++ src/main/java/net/fabricmc/morrowindmod/MoonshineCollector.java
@@ -0,0 +1,2 @@
+package net.fabricmc.morrowindmod;public class MoonshineCollector {
+}
--- src/main/java/net/fabricmc/morrowindmod/MorrowindMod.java
+++ src/main/java/net/fabricmc/morrowindmod/MorrowindMod.java
@@ -2,8 +2,12 @@ package net.fabricmc.morrowindmod;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.sound.SoundEvent;
@@ -14,18 +18,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MorrowindMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(net.fabricmc.morrowindmod.MorrowindMod.MOD_ID);
-
 	public static final String MOD_ID = "morrowindmod";
-	// an instance of our new item
-	public static final Item CUSTOM_ITEM =
-			Registry.register(Registry.ITEM, new Identifier(MOD_ID, "dank_kush"),
-					new Item(new FabricItemSettings()));
-
-
 	public static final FoodComponent SKOOMA_FOODCOMPONENT = new FoodComponent.Builder()
 			.hunger(-4)
 			.saturationModifier(0.6F)
@@ -48,14 +42,20 @@ public class MorrowindMod implements ModInitializer {
 			new MoonSugar(new FabricItemSettings().food(MOONSUGAR_FOODCOMPONENT).rarity(Rarity.UNCOMMON)));
 	public static final Identifier SKOOMA_SOUND_ID = new Identifier(MOD_ID, "skooma_sound");
 	public static final Identifier MOONSUGAR_SOUND_ID = new Identifier(MOD_ID, "moonsugar_sound");
-	public static final SoundEvent SKOOMA_SOUND = Registry.register(Registry.SOUND_EVENT, net.fabricmc.morrowindmod.MorrowindMod.SKOOMA_SOUND_ID, new SoundEvent(SKOOMA_SOUND_ID));
+	public static final SoundEvent SKOOMA_SOUND = Registry.register(Registry.SOUND_EVENT, SKOOMA_SOUND_ID, new SoundEvent(SKOOMA_SOUND_ID));
 
-	public static final SoundEvent MOONSUGAR_SOUND = Registry.register(Registry.SOUND_EVENT, net.fabricmc.morrowindmod.MorrowindMod.MOONSUGAR_SOUND_ID, new SoundEvent(MOONSUGAR_SOUND_ID));
+	public static final SoundEvent MOONSUGAR_SOUND = Registry.register(Registry.SOUND_EVENT, MOONSUGAR_SOUND_ID, new SoundEvent(MOONSUGAR_SOUND_ID));
 
 	//private static final ItemGroup ITEM_GROUP = FabricItemGroup.builder(new Identifier(ModName, String.format("%1$s_group",ModName)))
 	//		.icon(() -> new ItemStack(SKOOMA))
 	//		.build();
 
+	public static final Identifier MOONSHINECOLLECTOR_ID = new Identifier(MOD_ID, "moonshinecollector");
+
+
+	public static final Block MoonShineCollector = Registry.register(Registry.BLOCK, MOONSHINECOLLECTOR_ID, new Block(FabricBlockSettings.of(Material.AMETHYST).strength(1.0f)));
+
+	public static final Item MOONSHINECOLLECTOR_ITEM = Registry.register(Registry.ITEM, MOONSHINECOLLECTOR_ID, new BlockItem(MoonShineCollector, new FabricItemSettings()));
 
 	@Override
 	public void onInitialize() {
